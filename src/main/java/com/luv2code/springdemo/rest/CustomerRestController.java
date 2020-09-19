d4e190c1164536db47b00d8e2dc78d8076410a16
Ranjeet <ranjeetmdl@gmail.com>
2020-09-20 01:51:02 +0530

    section 67: add PUT api to update an existing customer

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.service.CustomerService;
+
 
 @RestController
 @RequestMapping("/api")
@@ -44,10 +46,18 @@ public class CustomerRestController {
 	//add a mapping for POST /customers
 	@PostMapping("/customers")
 	public Customer addCustomer(@RequestBody Customer theCustomer){
-		//set the customer id explicitly to 0, so to insert a new entry each time
-		//rest client call this API (bec of functionality of dao.saveOrUpdate())
+		//set the customer id explicitly to 0 in case if JSON  from the client has id, 
+		//so to insert a new entry each time (bec of functionality of dao.saveOrUpdate())
 		
 		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
+		return theCustomer;
+	}
+	
+	//add a mapping for PUT /customers
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer){
 		
 		customerService.saveCustomer(theCustomer);
 		return theCustomer;
